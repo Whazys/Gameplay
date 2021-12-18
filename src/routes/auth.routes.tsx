0b018c7d6e9410fc27bbb6cd0ebf44b4601dd673
whazys <santos_rafa1973@hotmail.com>
2021-12-18 19:31:57 -0300

    Nlw-6ª ReactNative

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import { Home } from '../screens/Home'
+import { SignIn } from '../screens/SignIn'
+
+const { Navigator, Screen } =  createNativeStackNavigator ()
+
+export function AuthRoutes () {
+    return (
+        <Navigator
+            headerMode="none"
+            screenOptions={{
+                card
+            }}
+        >
+            <Screen
+                name="SignIn"
+                component={SignIn}
+            />
+
+            <Screen
+                name="Home"
+                component={Home}
+            />
+
+        </Navigator>
+    )
+}
